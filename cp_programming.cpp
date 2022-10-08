ed1f6ece98f231a1941aa3590b897ebacfe9ebb4
Md Tauseef Akhtar <63928008+mdtauseef123@users.noreply.github.com>
2022-10-08 14:52:58 +0530

    Create cp_programming.cpp

---
+++ cp_programming.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define REP(i,a,b) for (int i=a;i<=b;i++)
+
+
+using namespace std;
+
+void rsolve(){
+    
+}
+
+void solve(){
+    int t;
+    cin>>t;
+    while(t--){
+        rsolve();
+    }
+}
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  solve();
+}
